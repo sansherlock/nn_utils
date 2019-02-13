c7b05ae59c0f4da37e787deb0784812ff9a0ec8e
liu <1131970238@qq.com>
2019-02-13 20:00:21 +0800

    first commit 20190213

---
+++ RunNN.py
@@ -0,0 +1,29 @@
+from nn_utils import *
+import numpy as np
+
+# a = np.array([[0.8,0.1],[0.2,0.7],[0.2,0.1]])
+# b = np.array([[1,0],[0,1],[0,0]])
+#
+# a1 = np.array([[0.9,0.05],[0.1,0.8],[0.2,0.1]])
+# b1 = np.array([[1,0],[0,1],[0,0]])
+#
+# cost = cost_func_cross_entropy(a,b,2)
+# cost1 = cost_func_cross_entropy(a1,b1,2)
+# print(cost)
+# print(cost1)
+
+m = 10
+cla = 10
+layer = [5,20,15,cla]
+learning_rate = 0.01
+layer_num = len(layer)
+x = np.random.rand(5,m)
+y = np.random.randint(0,cla,m)
+y = one_hot(y,cla,m)
+weights = init_weights(layer,layer_num)
+for i in range(500):
+    output,catches = forwardprop(x,weights,layer_num)
+    cost = cost_func_cross_entropy(output,y,m)
+    print(cost)
+    weights_derivatives = backprop(output,y,weights,catches,layer_num)
+    weights = update_weights(weights,weights_derivatives,learning_rate,layer_num)
---
+++ nn_utils.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def relu(z_data):
+    return np.maximum(z_data,0)
+
+def relu_derivative(data):
+    return 1.0*(data > 0)
+
+def sigmoid(z_data):
+    return 1/(1.0+np.exp(-z_data))
+
+def sigmoid_derivative(data):
+    return sigmoid(data) * (1 - sigmoid(data))
+
+def tanh(z_data):
+    return (np.exp(z_data) - np.exp(-1 * z_data)) / \
+             (np.exp(z_data) + np.exp(-1 * z_data))
+
+def softmax(z_data):
+    sub_sum = np.sum(np.exp(z_data),axis = 0)
+    a_data = np.exp(z_data) / sub_sum
+    return a_data
+
+def fc_layer(input,w,b):
+    return np.dot(w,input) + b
+
+def forwardprop(x,weights,layer_num):
+    catches = {}
+    catches['A0'] = x
+    for i in range(layer_num-2):
+        catches['Z'+str(i+1)] = fc_layer(catches['A'+str(i)],weights['w'+str(i+1)],weights['b'+str(i+1)])
+        catches['A'+str(i+1)] = relu(catches['Z'+str(i+1)])
+
+    catches['Z'+str(layer_num-1)] = fc_layer(catches['Z'+str(layer_num-2)],weights['w'+str(layer_num-1)],weights['b'+str(layer_num-1)])
+    catches['A'+str(layer_num-1)] = softmax(catches['Z'+str(layer_num-1)])
+    return catches['A'+str(layer_num-1)],catches
+
+def cost_func_cross_entropy(y,label,m):
+    return np.sum(-1/m * np.nan_to_num(label * np.log(y) + (1 - label) * np.log(1 - y)))
+
+def backprop(output,y,weights,catches,layer_num):
+    weights_derivatives = {}
+
+    delta = output - y
+    weights_derivatives['dw'+str(layer_num-1)] = np.dot(delta,catches['A'+str(layer_num-2)].transpose())
+    weights_derivatives['db'+str(layer_num-1)] = delta
+    for l in range(layer_num-2):
+        Z = catches['Z'+ str((layer_num-2)-l)]
+        sp = relu_derivative(Z)
+        delta = np.dot(weights['w'+str((layer_num-2)-l+1)].transpose(), delta) * sp
+        weights_derivatives['db'+str((layer_num-2)-l)] = delta
+        weights_derivatives['dw'+str((layer_num-2)-l)] = np.dot(delta, catches['A'+str((layer_num-2)-l-1)].transpose())
+    return weights_derivatives
+def update_weights(weights,weights_derivatives,learning_rate,layer_num):
+    for i in range(layer_num-1):
+        weights['w'+str(i+1)] = weights['w'+str(i+1)] - learning_rate * weights_derivatives['dw'+str(i+1)]
+        weights['b'+str(i+1)] = weights['b'+str(i+1)] - learning_rate * weights_derivatives['db'+str(i+1)]
+    return weights
+
+def init_weights(layer,layer_num):
+    weights = {}
+    for i in range(layer_num-1):
+        weights['w'+str(i+1)] = np.random.rand(layer[i+1],layer[i])
+        weights['b'+str(i+1)] = np.random.rand(layer[i+1],1)
+    return weights
+
+def one_hot(label,cla,m):
+    one_hot_label = np.zeros((cla,m))
+    # 方式1
+    for i in range(m):
+        one_hot_label[label[i],i] = 1
+    return one_hot_label
+    # 方式2
+    one_hot_label = np.eye(cla)[label].T
+    return one_hot_label
